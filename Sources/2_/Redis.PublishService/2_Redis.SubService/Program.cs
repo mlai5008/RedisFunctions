028fb91cef86286a2d01bacca89de6afb7a79e05
Yura <Yura@DELLYKPC>
2025-12-05 11:16:33 +0300

    Add second repository

---
+++ Sources/2_/Redis.PublishService/2_Redis.SubService/Program.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+
+namespace _2_Redis.SubService
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string configuration = "localhost:6379,abortConnect=false,ssl=true";
+            const string channelName = "RedisChannel";
+            IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(configuration);
+            var subscriber = connectionMultiplexer.GetSubscriber();
+
+            await subscriber.SubscribeAsync(RedisChannel.Literal(channelName), (channel, json) =>
+            {
+                var message = JsonSerializer.Deserialize<(Guid, DateTime)>(json);
+                Console.WriteLine($"{message.Item1} {message.Item2}");
+            });
+
+            Console.WriteLine("Press key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sources/2_/Redis.PublishService/Redis.PubSub/Program.cs
@@ -0,0 +1,36 @@
+﻿using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Redis.PubSub
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Press <Enter> to exit...");
+            Console.WriteLine("Press <P> to publish message.");
+            const string configuration = "localhost:6379,abortConnect=false,ssl=true";
+            const string channelName = "RedisChannel";
+            IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(configuration);
+            var subscriber = connectionMultiplexer.GetSubscriber();
+
+            while (true)
+            {
+                var keiKey = Console.ReadKey().Key;
+                if (keiKey == ConsoleKey.P)
+                {
+                    (Guid, string) message = new(Guid.NewGuid(), DateTime.UtcNow.ToString());
+                    var json = JsonSerializer.Serialize(message);
+                    await subscriber.PublishAsync(RedisChannel.Literal(channelName), json);
+                    Console.WriteLine($"{message.Item1} {message.Item2}");
+                }
+                if (keiKey == ConsoleKey.Enter)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("EXIT!");
+        }
+    }
+}
